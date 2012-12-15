590e9d77b078421a0c1e9db7a2c7b32967798d81
Den_dp <Denis.Bendrikov@gmail.com>
2012-12-15 23:30:04 +0200

    final version

--- www/lab5/dataProcessing.pl
+++ www/lab5/dataProcessing.pl
@@ -1,9 +1,8 @@
-#!/usr/local/miniperl/miniperl
+#!/usr/bin/perl -w
 
-#use strict;
-#use warnings;
-#use 5.010;
-#use utf8;
+use strict;
+use URI::Escape;
+# use utf8;
 
 # Лабораторна робота №5
 #
@@ -16,8 +15,29 @@
 # Для обробки даних, які переслані на сервер, створити CGI-скрипт на мові Perl.
 # Розкодовані дані повернути клієнтові
 
-
 print "Content-type: text/html\n\n";
-print "<html><br><br><br><br><h2>!!!</h2>";
-print "Thank you, here is the information you entered:<br>";
-print "Name: $name";
+print "<!DOCTYPE html><html><head>\n";
+print "<title>Result</title>\n";
+print "</head><body>\n";
+
+my (@formFields, $query);
+
+if ($ENV{REQUEST_METHOD} eq "GET") {
+	$query = uri_unescape($ENV{'QUERY_STRING'});
+} else {
+	read STDIN, $query, $ENV{CONTENT_LENGTH};
+	$query = uri_unescape($query);
+}
+
+if ($query) {
+	@formFields = split(/&/,$query);
+	print "Passed data:<ul>";
+	foreach (@formFields) {
+		if (/^(\w+)=(.*)/) {
+			print "<li>",$1," = ",$2,"</li>";
+		}
+	}
+	print "</ul>";
+}
+
+print "</body></html>";
